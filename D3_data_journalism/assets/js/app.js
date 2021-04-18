ace912a1428ecf8d5c97034ada43739a737b2ec3
Bismaad Minhas <bismaad@gmail.com>
2021-04-18 12:24:50 -0400

    wrap function created to make code responsive to window size

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,15 @@
-// @TODO: YOUR CODE HERE!
+// Wrap code inside a function that will automatically
+// resize the visuals based on the window size
+
+function resizeResponse(){
+
+
+
+}
+
+// Call resize function when browser is loads
+resizeResponse ();
+
+// Call rezie function when the window is resized
+d3.select(window).("resize", resizeResponse);
+
